28b4df074622d9b2c587d189cbab2a397196db5d
uzaifkhan-2774 <uzaifkhan248@gmail.com>
2026-04-16 19:35:26 +0530

    update env file by url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://taski-backend-4.onrender.com/api',
+  baseURL: import.meta.env.VITE_API_URL || 'https://taski-backend-booking.onrender.com/api',
 })
 
 api.interceptors.request.use((config) => {
