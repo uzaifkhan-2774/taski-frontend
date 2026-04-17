9371fac275fbc636fca290bd2a09a447623ffafa
uzaifkhan-2774 <uzaifkhan248@gmail.com>
2026-04-17 18:35:16 +0530

    some update

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://taski-backend-5.onrender.com/api'
+  baseURL: import.meta.env.VITE_API_URL || 'https://taski-backend-flbook.onrender.com/api'
 })
 
 api.interceptors.request.use((config) => {
